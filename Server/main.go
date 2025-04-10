9d5401f414b9260012beac87727b1fd7d1a868c6
Literally Kirby <soniciscool12322gmail.comgit config --global user.email soniciscool12322gmail.comgit config --global user.email soniciscool12322gmail.com>
2025-04-10 13:47:56 -0500

    Added Chat Function and Quiting

--- Server/main.go
+++ Server/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
+	"math/rand/v2"
+	"strconv"
+	"strings"
+
+	//"log"
 	"net"
 )
 
@@ -28,22 +32,64 @@ func server() {
 	}
 }
 
+var clients = make(map[net.Conn]string)
+
+var commands = []string{"Quit", "ChangeNick", "ListUsers", "Help"}
+
 func handleConnection(conn net.Conn) {
 	fmt.Println("Client connected:", conn.RemoteAddr())
 	defer conn.Close()
 
 	// Create a buffered reader and read the incoming message
 	reader := bufio.NewReader(conn)
-	message, err := reader.ReadString('\n') // Capture the message here
-	if err != nil {
-		log.Println("Error reading from connection:", err)
-		return
+	conn.Write([]byte("Welcome to the server!\n"))
+	conn.Write([]byte("Please enter a Username:\n"))
+	nickname, _ := reader.ReadString('\n') // Capture the nickname
+	trimmedNickname := strings.TrimSpace(nickname)
+	if trimmedNickname == "" {
+		rand := strconv.Itoa(rand.IntN(1000)) // Generate a random number
+		anonUsername := "Anon" + rand         // Default to "Anon" if no nickname is provided
+		nickname = anonUsername
+	} else {
+		nickname = nickname[:len(nickname)-1] // Remove the newline character
 	}
+	clients[conn] = nickname // Add the client to the map with their nickname
 
-	// Log the message received from the client (optional)
-	log.Println("Received:", message)
+	BroadcastMessage(fmt.Sprintf("%s has joined the chat\n", nickname))
 
-	// Send a response back to the client
-	response := "Hello from server!"
-	conn.Write([]byte(response))
+	for {
+		message, _ := reader.ReadString('\n')
+		trimmed := strings.TrimSpace(message)
+		if trimmed != "" {
+			cleanedMessage := message[:len(message)-1] // Remove the newline character
+			firstChar := cleanedMessage[0]
+			if firstChar == '/' {
+				switch cleanedMessage {
+				case "/quit":
+					conn.Close()
+					delete(clients, conn) // Remove the client from the map
+					goodbyeMessage := nickname + " has left the chat\n"
+					BroadcastMessage(goodbyeMessage)
+				case "/nick":
+
+				}
+			} else {
+				messageToSend := nickname + ": " + cleanedMessage + "\n"
+				BroadcastMessage(messageToSend)
+			}
+
+		} else {
+			continue
+		}
+	}
+	conn.Close()
+	delete(clients, conn) // Remove the client from the map
+	goodbyeMessage := nickname + " has left the chat\n"
+	BroadcastMessage(goodbyeMessage)
+}
+
+func BroadcastMessage(message string) {
+	for conn := range clients {
+		conn.Write([]byte(message))
+	}
 }
